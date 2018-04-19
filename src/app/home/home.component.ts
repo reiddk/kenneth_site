2507555a464b38f5d99b84951ebb7432e44623a1
osboxes.org <osboxes@osboxes>
2018-04-19 05:50:11 +0100

    made page view per book

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -11,7 +11,10 @@ import { Subscription } from 'rxjs/Subscription';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-
+/*
+  <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12 pdf-div" *ngFor="let pdf of lineSettings.link_output"><a (click)="showPdf(pdf.pdf_link, pdf.name, pdf?.pdf_image)" class="pdf-link"><div><img src="{{pdf?.pdf_image}}" style="width:100%;"></div><h2 class="pdf-name-wrapper">{{pdf.name}}
+  </h2></a></div>
+*/
 
 export class HomeComponent implements OnInit, OnDestroy {
 
@@ -24,25 +27,44 @@ export class HomeComponent implements OnInit, OnDestroy {
   	private parseService: ParseService,
     public router: Router) { }
 
+  getPDFS(name) {
+    name = decodeURIComponent(name);
+    let outData = {};
+    for (var i = 0; i < this.pageSettings.length; i++) {
+      if (this.pageSettings[i].type === "pdfs") {
+        for (var y = 0; y < this.pageSettings[i].link_output.length; y++) {
+          if (this.pageSettings[i].link_output[y].name === name) {
+            this.storeService.passPdfToShow({'link':this.pageSettings[i].link_output[y].pdf_link, 'name': this.pageSettings[i].link_output[y].name});
+          }
+        }
+      }
+    }
+  }
+
   updateView() {
     const self = this;
     self.pageSettings = [];
     let routerVar = self.router.url;
+    let book = routerVar.split("/");
     if (typeof routerVar !== 'string' || routerVar === '/' || routerVar === '') {
       routerVar = '/home';
-    }
-    if (self.storeService.titleToLink[routerVar]) {
-      self.requestService.get(self.storeService.titleToLink[routerVar], 'text')
+    } 
+
+      self.requestService.get(self.storeService.titleToLink['/home'], 'text')
       .then((data) => {
         self.hackyTempBool = true;
         self.pageSettings = self.parseService.parseFile(data);
         console.log(self.pageSettings);
+        if (routerVar.indexOf('book') > -1) {
+          let name = routerVar.split('/')[2];
+          this.getPDFS(name);
+        }
       })
       .catch((e) => { 
         console.log('there was an error');
         console.log(e);
        }); 
-    }
+    
   }
 
   ngOnInit() {
